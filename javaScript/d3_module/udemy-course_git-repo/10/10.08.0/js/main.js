658976de3eb1c174bafe181d619b1a157bd76377
Bharath Deva <dbharath27@gmail.com>
2020-09-07 21:39:49 +0530

    add changes in repo's tree

--- javaScript/d3_module/udemy-course_git-repo/10/10.08.0/js/main.js
+++
@@ -1,9 +0,0 @@
-/*
-*    main.js
-*    Mastering Data Visualization with D3.js
-*    FreedomCorp Dashboard
-*/
-
-d3.json("data/calls.json").then(function(data){    
-    console.log(data)
-})
--- typeScript/app.js
+++
@@ -1,9 +0,0 @@
-function add(n1, n2) {
-    return n1 + n2;
-}
-var n = "5";
-var g = {
-    name: "BHARATH",
-    AGE: "26"
-};
-console.log(g.AGE);
